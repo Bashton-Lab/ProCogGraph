bbd49cc142765852ed62402c5348cfcd1000532c
m-crown <30960941+m-crown@users.noreply.github.com>
2024-02-01 10:10:41 +0000

    add process ec_records func to utils

--- utils.py
+++ utils.py
@@ -7,6 +7,65 @@ import requests
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 import json 
+from Bio.ExPASy import Enzyme as EEnzyme
+
+#make this function be applicable to extract_pdbe_info script too - need to check if the grouped output at end is appropriate.
+def process_ec_records(enzyme_dat_file, enzyme_class_file):
+    """
+    Process EC records and generate related dataframes.
+
+    Returns:
+    ec_records_df_grouped (DataFrame): Grouped EC records dataframe.
+    ec_class_descriptions (DataFrame): EC class descriptions dataframe.
+    ec_subclass_descriptions (DataFrame): EC subclass descriptions dataframe.
+    ec_subsubclass_descriptions (DataFrame): EC subsubclass descriptions dataframe.
+    """
+
+    with open(enzyme_dat_file) as handle:
+        ec_records = EEnzyme.parse(handle)
+        ec_records_list = []
+        for record in ec_records: 
+            ec_record_series = pd.Series(record)
+            ec_records_list.append(ec_record_series)
+
+    ec_records_df = pd.DataFrame(ec_records_list)
+    ec_records_df["TRANSFER"] = ec_records_df.apply(lambda x: get_terminal_record(x["ID"], x, ec_records_df), axis = 1)
+    ec_records_df["TRANSFER"] = ec_records_df["TRANSFER"].fillna(ec_records_df.ID)
+
+    with open(enzyme_class_file, 'r') as file:
+        lines = [line.strip() for line in file if re.match(r'^\d\.', line.strip())]
+
+    ec_descriptions = []
+    for line in lines:
+        ec = ''.join(line[0:9]).replace(' ', '')
+        description = re.findall(r'.*.-\s+(.*)', line)[0]
+        ec_descriptions.append((ec, description))
+
+    ec_descriptions_df = pd.DataFrame(ec_descriptions, columns= ["EC", "description"])
+    ec_records_df_grouped = ec_records_df.groupby("TRANSFER").agg({"ID": list}).reset_index()
+
+    ec_records_df_grouped = ec_records_df_grouped.merge(ec_records_df[["ID", "DE"]], left_on = "TRANSFER", right_on = "ID")
+    ec_records_df_grouped.rename(columns = {"ID_x" : "ID"}, inplace = True)
+    ec_records_df_grouped.drop(columns = ["ID_y"], inplace = True)
+    ec_records_df_grouped[["class", "subclass", "subsubclass", "term"]] = ec_records_df_grouped.TRANSFER.str.split(".", expand = True)
+
+    subsubclass = ec_records_df_grouped["class"].astype("str") + "." + ec_records_df_grouped["subclass"] + "." + ec_records_df_grouped["subsubclass"] + ".-"
+    subclass = ec_records_df_grouped["class"].astype("str") + "." + ec_records_df_grouped["subclass"] + ".-.-"
+    ec_class = ec_records_df_grouped["class"].astype("str") + ".-.-.-"
+
+    ec_records_df_grouped["subsubclass"] = subsubclass
+    ec_records_df_grouped["subclass"] = subclass
+    ec_records_df_grouped["class"] = ec_class
+
+    ec_class_descriptions = ec_descriptions_df.loc[ec_descriptions_df.EC.str.contains("\d\.-\.-\.-$", regex = True)].copy().rename(columns = {"EC": "class","description" : "class_description"})
+    ec_subclass_descriptions = ec_descriptions_df.loc[ec_descriptions_df.EC.str.contains(r"\d\.-\.-$", regex = True)].copy().rename(columns = {"EC":"subclass","description" : "subclass_description"})
+    ec_subsubclass_descriptions = ec_descriptions_df.loc[ec_descriptions_df.EC.str.contains(r"\d\.-$", regex = True)].copy().rename(columns = {"EC" : "subsubclass","description" : "subsubclass_description"})
+
+    ec_records_df_grouped = ec_records_df_grouped.merge(ec_class_descriptions, on = "class", how = "left")
+    ec_records_df_grouped = ec_records_df_grouped.merge(ec_subclass_descriptions, on = "subclass", how = "left")
+    ec_records_df_grouped = ec_records_df_grouped.merge(ec_subsubclass_descriptions, on = "subsubclass", how = "left")
+
+    return ec_records_df_grouped
 
 #this is a core function - should be stored elsewhere.
 #a limitation of this function is that it selects only one representative EC number for transferred entries
