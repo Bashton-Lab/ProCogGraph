d64229c9c8dd17be1bc048c8ca0e2980443d0069
m-crown <30960941+m-crown@users.noreply.github.com>
2024-02-27 23:55:13 +0000

    make internal paralellisation main, del snakemake

--- get_pdb_parity.py
+++ get_pdb_parity.py
@@ -15,107 +15,124 @@ from collections import Counter
 import rdkit.Chem as Chem
 from rdkit.Chem import BRICS,Recap, rdFMCS
 from rdkit.Chem import Draw
+
+import pickle
 import os
 import math
 import numpy as np
 from math import isnan
-from pdbeccdutils.computations.parity_method import compare_molecules    
+import concurrent.futures
+from pathlib import Path
+#need to track whether a molecules MCS was timeouted or not, and then we can collect and repeat these structures 
+
+from pdbeccdutils.computations.parity_method import compare_molecules
+    
+import io
 from contextlib import redirect_stderr
 
-def parity_score_smiles(pdb_ligand_id, smiles, ec, bl_name, ligand_description, compound_df_subset, threshold):
+def parity_score_smiles(row, threshold, progress, task): #, 
+    ec = row.entry
+    pdb_ligand_id = row.pdb_ligand_id
+    smiles = row.smiles
+    bl_name = row.bl_name
+    ligand_description = row.ligand_description
+    cognate_ligand_smiles = row.canonical_smiles
     f = io.StringIO()
-    scores = []
     
     ec = ",".join(ec)
-    if len(compound_df_subset) == 0:
-        score_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": None, "score" : 0, "error" : f"No biological compounds found for ligand", "cancelled" : None}
-        scores.append(score_dict)
+    if cognate_ligand_smiles == None:
+        scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": None, "score" : 0, "error" : f"No biological compounds found for ligand", "cancelled" : None}
+        return scores_dict
     else:
         with redirect_stderr(f):
             try:
                 #repeat canonicalisation to ensure best possible parity score
                 ligand_rdkit = Chem.MolFromSmiles(Chem.CanonSmiles(smiles))
             except Exception as e:
-                for idx, row in compound_df_subset.iterrows():
-                    compound = row["canonical_smiles"]
-                    score_dict = {"ec" : ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": compound, "score" : 0, "error" : f"PDB Ligand error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0}
-                    scores.append(score_dict)
-                return scores
+                scores_dict = {"ec" : ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": cognate_ligand_smiles, "score" : 0, "error" : f"PDB Ligand error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0}
+                return scores_dict
 
         out = f.getvalue()
-        for idx, row in compound_df_subset.iterrows():
-            compound = row["canonical_smiles"]
-            ec = ",".join(row["entry"])
-            try:
-                rdkit_compound = row["ROMol"]
-                if rdkit_compound in [None, np.nan]:
-                    scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": compound, "score" : 0, "error" : f"RDKit compound not found for compound", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0}
-                else:
-                    #repeat canonicalisation to ensure best possible parity score
-                    rdkit_compound = Chem.MolFromSmiles(Chem.CanonSmiles(row["canonical_smiles"]))
-                    parity = compare_molecules(ligand_rdkit, rdkit_compound, thresh = threshold)
-                    score = parity.similarity_score
-                    mol1_atom_count = ligand_rdkit.GetNumAtoms()
-                    mol2_atom_count = rdkit_compound.GetNumAtoms()
-                    matching_atoms = len(parity.mapping)
-                    pdbl_subparity = matching_atoms/mol1_atom_count
-                    bl_subparity = matching_atoms/mol2_atom_count
-                    cancelled = None
-                    #score, pdbl_subparity, bl_subparity, cancelled = get_parity_score(ligand_rdkit, rdkit_compound, returnmcs=False, timeout = timeout)
-                    scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": compound, "score" : score, "error" : None, "cancelled" : cancelled, "pdbl_subparity": pdbl_subparity, "bl_subparity": bl_subparity}
-                scores.append(scores_dict)
-            except Exception as e:
-                scores_dict = {"ec": ec,"pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": compound, "score" : 0, "error" : f"Parity error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0}
-                scores.append(scores_dict)
-    
-    return scores
+        
+        ec = ",".join(row["entry"])
+        try:
+            rdkit_compound = Chem.MolFromSmiles(Chem.CanonSmiles(cognate_ligand_smiles))
+            if rdkit_compound in [None, np.nan]:
+                scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": cognate_ligand_smiles, "score" : 0, "error" : f"RDKit compound not found for compound", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0}
+            else:
+                #repeat canonicalisation to ensure best possible parity score
+                parity = compare_molecules(ligand_rdkit, rdkit_compound, thresh = threshold)
+                score = parity.similarity_score
+                mol1_atom_count = ligand_rdkit.GetNumAtoms()
+                mol2_atom_count = rdkit_compound.GetNumAtoms()
+                matching_atoms = len(parity.mapping)
+                pdbl_subparity = matching_atoms/mol1_atom_count
+                bl_subparity = matching_atoms/mol2_atom_count
+                cancelled = None
+                #score, pdbl_subparity, bl_subparity, cancelled = get_parity_score(ligand_rdkit, rdkit_compound, returnmcs=False, timeout = timeout)
+                scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": cognate_ligand_smiles, "score" : score, "error" : None, "cancelled" : cancelled, "pdbl_subparity": pdbl_subparity, "bl_subparity": bl_subparity}
+        except Exception as e:
+            scores_dict = {"ec": ec,"pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": cognate_ligand_smiles, "score" : 0, "error" : f"Parity error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0}
+    progress.update(task, advance=1)
+    return scores_dict
 
+def get_compound_pairs(row, cognate_ligands_df):
+    ec = row.ec_list
+    pdb_ligand_id = row.ligand_entity_id
+    smiles = row.descriptor
+    bl_name = row.bl_name
+    ligand_description = row.description
+    cognate_ligands_df_subset = cognate_ligands_df.loc[(cognate_ligands_df.entry.isin(ec)), ["entry", "canonical_smiles", "ROMol"]].copy()
+    cognate_ligands_df_subset = cognate_ligands_df_subset.groupby("canonical_smiles").agg({"entry" : list, "ROMol" : "first"}).reset_index()
+    cognate_ligands_df_subset[["pdb_ligand_id", "smiles", "bl_name", "ligand_description"]] = [pdb_ligand_id, smiles, bl_name, ligand_description]
+    return cognate_ligands_df_subset
 
 import argparse
+from rdkit import RDLogger
+import time
 
 parser = argparse.ArgumentParser(description = 'TO DO')
 parser.add_argument('--processed_ligands_file', metavar = '', type = str, 
-    help = "File of PDB ligands and their associated EC numbers to score")
+    help = "")
 parser.add_argument('--cognate_ligands_file', metavar = '', type = str, 
-    help = "File of biological ligands and their associated EC numbers to score")
+    help = "")
 parser.add_argument('--outdir', metavar = 'parity_calcs', type = str, 
-    help = "Output directory")
-parser.add_argument('--chunk', metavar = '', type = int, 
-    help = "Chunk index")
-parser.add_argument('--chunk_size', metavar = '', type = int, default= 100,
-    help = "Chunk size")
-parser.add_argument('--threshold', metavar = '', type = float, default= 0.1,
-    help = "Threshold for PARITY score calculation.")
+    help = "")
+parser.add_argument('--threshold', metavar = '', type = float, default = 0.1,
+    help = "")
+parser.add_argument('--threads', metavar = '', type = int, default = 1,
+    help = "")
 
+start = time.time()
 args = parser.parse_args()
-
+RDLogger.DisableLog('rdApp.*') 
 all_chem_descriptors_ligands_unique_pairs = pd.read_pickle(f"{args.processed_ligands_file}")
-
 cognate_ligands_df = pd.read_pickle(f"{args.cognate_ligands_file}")
-chunk_index = args.chunk
-chunk_size = args.chunk_size
 
+pickle_filename = f"{args.outdir}/all_parity_calcs.pkl"
 
-start_index = chunk_index * chunk_size
-end_index = start_index + chunk_size
-chunk_df = all_chem_descriptors_ligands_unique_pairs[start_index:end_index]
-pickle_filename = f"{args.outdir}/parity_calcs_chunk_{chunk_index}.pkl"
-
-
+Path(pickle_filename).parent.mkdir(parents=True, exist_ok=True)
 if not os.path.exists(pickle_filename):
-    chunk_results = []
-    for index, row in chunk_df.iterrows():
-        bl_name = row['bl_name']
-        ligand_id = row['ligand_entity_id']
-        ligand_representation = row['descriptor']
-        ligand_description = row["description"]
-        ec = row['ec_list']
+    all_pairs = []
+    for index, row in all_chem_descriptors_ligands_unique_pairs.iterrows():
+        pairs = get_compound_pairs(row, cognate_ligands_df)
+        all_pairs.append(pairs)
+
+    all_pairs_df = pd.concat(all_pairs, ignore_index = True)
+    results = []
+    with Progress() as progress:
+        task = progress.add_task("[red]Calculating parity scores...", total=len(all_pairs_df))
         
-        cognate_ligands_df_subset = cognate_ligands_df.loc[(cognate_ligands_df.entry.isin(ec)), ["entry", "canonical_smiles", "ROMol"]].copy()
-        cognate_ligands_df_subset = cognate_ligands_df_subset.groupby("canonical_smiles").agg({"entry" : list, "ROMol" : "first"}).reset_index()
-        scores_list = parity_score_smiles(ligand_id, ligand_representation, ec, bl_name, ligand_description,cognate_ligands_df_subset, args.threshold)
-        chunk_results.extend(scores_list)
-        print(f"Chunk {args.chunk}, row {index}")
-    # Save the smiles_ec_pairs for the chunk as a pickle file
-    with open(pickle_filename, "wb") as file:
-        pickle.dump(chunk_results, file)
+        with concurrent.futures.ThreadPoolExecutor(args.threads) as executor:
+            futures = []
+            for index, row in all_pairs_df.iterrows():
+                futures.append(executor.submit(parity_score_smiles, row, args.threshold, progress,task))
+            for future in concurrent.futures.as_completed(futures):
+                results.extend([future.result()])
+                futures.remove(future)
+
+        results_df = pd.DataFrame(results)
+        # Save the smiles_ec_pairs for the chunk as a pickle file
+        results_df.to_pickle(pickle_filename)
+end = time.time()
+print(f"Time taken: {end - start} seconds")
