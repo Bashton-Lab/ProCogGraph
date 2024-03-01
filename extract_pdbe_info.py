2126f8ab2e54963105700428f6dc202d1df77aa5
m-crown <30960941+m-crown@users.noreply.github.com>
2024-03-01 17:22:46 +0000

    group uniprot accessions at chain level

--- extract_pdbe_info.py
+++ extract_pdbe_info.py
@@ -295,8 +295,9 @@ def main():
 
         sifts_chains = pd.read_csv(f"{args.sifts_ec_mapping}", sep = "\t", comment="#")
         sifts_chains_ec = sifts_chains.loc[sifts_chains.EC_NUMBER != "?"]
-        sifts_chains_ec = sifts_chains_ec.groupby(["PDB", "CHAIN", "ACCESSION"]).agg({"EC_NUMBER": list}).reset_index()
-        sifts_chains_ec["EC_NUMBER"] = sifts_chains_ec["EC_NUMBER"].apply(lambda x: ",".join(x)) #join the list of EC numbers into a single string for ec records function
+        sifts_chains_ec = sifts_chains_ec.groupby(["PDB", "CHAIN"]).agg({"ACCESSION": list, "EC_NUMBER": list}).reset_index() #some chains have multiple uniprot accessions, we group these into a list along with their associated ec's
+        sifts_chains_ec["EC_NUMBER"] = sifts_chains_ec["EC_NUMBER"].apply(lambda x: ",".join(x)) #join the list of EC numbers into a single string for ec records function 
+        sifts_chains_ec["ACCESSION"] = sifts_chains_ec["ACCESSION"].apply(lambda x: "|".join(x)) #join the list of uniprot accessions with a pipe for downstream neo4j integration
         sifts_chains_ec = get_updated_enzyme_records(sifts_chains_ec, ec_records_df, ec_col = "EC_NUMBER")
         sifts_chains_ec.rename(columns = {"EC_NUMBER": "protein_entity_ec", "ACCESSION" : "uniprot_accession"}, inplace = True)
         
