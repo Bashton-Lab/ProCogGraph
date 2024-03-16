3c1e1607e0890feb521c519aa8a067528de2e91b
m-crown <30960941+m-crown@users.noreply.github.com>
2024-03-16 20:08:01 +0000

    save mcs as smarts instead of svg

--- get_pdb_parity.py
+++ get_pdb_parity.py
@@ -45,7 +45,7 @@ def parity_score_smiles(row, threshold, progress, task, image_path): #,
     
     ec = ",".join(ec)
     if cognate_ligand_smiles == None:
-        scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": None, "score" : 0, "error" : f"No biological compounds found for ligand", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None}
+        scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "compound": None, "score" : 0, "error" : f"No biological compounds found for ligand", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None, "parity_smarts": None}
         return scores_dict
     else:
         with redirect_stderr(f):
@@ -53,7 +53,7 @@ def parity_score_smiles(row, threshold, progress, task, image_path): #,
                 #repeat canonicalisation to ensure best possible parity score
                 ligand_rdkit, ligand_sanitisation = pdbe_sanitise_smiles(smiles, return_mol = True, return_sanitisation=True)
             except Exception as e:
-                scores_dict = {"ec" : ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"PDB Ligand error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None}
+                scores_dict = {"ec" : ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"PDB Ligand error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None, "parity_smarts": None}
                 return scores_dict
 
         out = f.getvalue()
@@ -62,7 +62,7 @@ def parity_score_smiles(row, threshold, progress, task, image_path): #,
         try:
             rdkit_compound, compound_sanitisation = pdbe_sanitise_smiles(cognate_ligand_smiles, return_mol = True, return_sanitisation=True)
             if rdkit_compound in [None, np.nan]:
-                scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"RDKit compound not found for compound", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None}
+                scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"RDKit compound not found for compound", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None, "parity_smarts": None}
             else:
                 if ligand_sanitisation and compound_sanitisation:
                     #repeat canonicalisation to ensure best possible parity score
@@ -70,25 +70,14 @@ def parity_score_smiles(row, threshold, progress, task, image_path): #,
                     #re-run the mcs to save the graph - or can we highlight the resulting atoms from the dictioanry returned by compare_molecules
                     score = parity.similarity_score
                     parity_match = parity.mapping
-                    if score >= threshold:
-                        try:
-                            mcs_graph = rdFMCS.FindMCS(
+                    mcs_graph = rdFMCS.FindMCS(
                                 [ligand_rdkit, rdkit_compound],
                                 bondCompare=rdFMCS.BondCompare.CompareAny,
                                 atomCompare=rdFMCS.AtomCompare.CompareAny,
                                 timeout=40,
                                 completeRingsOnly=True,
                             )
-                            mcs = Chem.MolFromSmarts(mcs_graph.smartsString)
-                            mcsMolsMatrix = [[ligand_rdkit, mcs, rdkit_compound]]
-                            mcsLegends = [[f'PDB Ligand UID: {int(pdb_ligand_id)}', f"Parity Score: {round(score,3)}", f"Cognate Ligand UID: {int(cognate_ligand_id)}"]]
-                            mcsHighlightAtomLists = [[(parity_match.keys()), (), (parity_match.values())]]
-                            dos = Draw.MolDrawOptions()
-                            dos.legendFraction = 0.10
-                            img = Draw.MolsMatrixToGridImage(molsMatrix=mcsMolsMatrix, highlightAtomListsMatrix=mcsHighlightAtomLists, legendsMatrix=mcsLegends, returnPNG=True, drawOptions = dos)
-                            img.save(f'{image_path}/{int(pdb_ligand_id)}_{int(cognate_ligand_id)}_parity.png')
-                        except Exception: 
-                            pass
+                    mcs_smarts = mcs_graph.smartsString
                     
                     mol1_atom_count = ligand_rdkit.GetNumAtoms()
                     mol2_atom_count = rdkit_compound.GetNumAtoms()
@@ -97,11 +86,11 @@ def parity_score_smiles(row, threshold, progress, task, image_path): #,
                     bl_subparity = matching_atoms/mol2_atom_count
                     
                     cancelled = None
-                    scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : score, "error" : None, "cancelled" : cancelled, "pdbl_subparity": pdbl_subparity, "bl_subparity": bl_subparity, "parity_match": parity_match}
+                    scores_dict = {"ec": ec, "pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : score, "error" : None, "cancelled" : cancelled, "pdbl_subparity": pdbl_subparity, "bl_subparity": bl_subparity, "parity_match": parity_match, "parity_smarts": mcs_smarts}
                 else:
-                    scores_dict = {"ec": ec,"pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"Sanitisation error", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None}
+                    scores_dict = {"ec": ec,"pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"Sanitisation error", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None, "parity_smarts": None}
         except Exception as e:
-            scores_dict = {"ec": ec,"pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"Parity error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None}
+            scores_dict = {"ec": ec,"pdb_ligand" : pdb_ligand_id, "pdb_ligand_name": bl_name, "pdb_ligand_description": ligand_description, "cognate_ligand": cognate_ligand_id, "score" : 0, "error" : f"Parity error: {str(e)}", "cancelled" : None, "pdbl_subparity": 0, "bl_subparity": 0, "parity_match": None, "parity_smarts": None}
     progress.update(task, advance=1)
     return scores_dict
 
