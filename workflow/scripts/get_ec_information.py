792fda0dbcb869d936da143ceb67d06d8b566fc3
m-crown <30960941+m-crown@users.noreply.github.com>
2024-04-14 17:20:48 +0100

    join liganddb reaction ids before merge

--- workflow/scripts/get_ec_information.py
+++ workflow/scripts/get_ec_information.py
@@ -503,6 +503,7 @@ def main():
         kegg_reaction_enzyme_df_exploded_kegg = kegg_reaction_enzyme_df_exploded_kegg.dropna(subset = ["canonical_smiles"])
         kegg_reaction_enzyme_df_exploded_kegg["ligand_db"] = "KEGG:" + kegg_reaction_enzyme_df_exploded_kegg["entities"].astype("str")
         kegg_reaction_enzyme_df_exploded_kegg["compound_reaction"] = kegg_reaction_enzyme_df_exploded_kegg.apply(lambda x: [re.search(r"^(.+):", item)[1] for item in x.reaction_equation.split(",") if x.compound_id in item], axis = 1)
+        kegg_reaction_enzyme_df_exploded_kegg["compound_reaction"] = kegg_reaction_enzyme_df_exploded_kegg["compound_reaction"].str.join("|")
         PandasTools.AddMoleculeColumnToFrame(kegg_reaction_enzyme_df_exploded_kegg, smilesCol='canonical_smiles')
         kegg_reaction_enzyme_df_exploded_kegg.to_pickle(f"{args.outdir}/kegg_reaction_enzyme_df_exploded_kegg.pkl")
         print("KEGG Compound records merged with Enzyme and Reaction records and saved")
@@ -536,7 +537,7 @@ def main():
 
         kegg_reaction_enzyme_df_exploded_chebi["ligand_db"] = kegg_reaction_enzyme_df_exploded_chebi["ChEBI_ID"].astype("str")
         kegg_reaction_enzyme_df_exploded_chebi["compound_reaction"] = kegg_reaction_enzyme_df_exploded_chebi.apply(lambda x: [re.search(r"^(.+):", item)[1] for item in x.reaction_equation.split(",") if x["KEGG COMPOUND ACCESSION"] in item], axis = 1)
-
+        kegg_reaction_enzyme_df_exploded_chebi["compound_reaction"] = kegg_reaction_enzyme_df_exploded_chebi["compound_reaction"].str.join("|")
         kegg_reaction_enzyme_df_exploded_chebi = kegg_reaction_enzyme_df_exploded_chebi.loc[kegg_reaction_enzyme_df_exploded_chebi.ROMol.isna() == False].reset_index(drop = True)
         kegg_reaction_enzyme_df_exploded_chebi.to_pickle(f"{args.outdir}/kegg_reaction_enzyme_df_exploded_chebi.pkl")
         print("ChEBI records saved")
@@ -584,6 +585,7 @@ def main():
         kegg_reaction_enzyme_df_exploded_pubchem = kegg_reaction_enzyme_df_exploded_pubchem.loc[kegg_reaction_enzyme_df_exploded_pubchem.ROMol.isna() == False].reset_index(drop = True)
         kegg_reaction_enzyme_df_exploded_pubchem["ligand_db"] = "Pubchem:" + kegg_reaction_enzyme_df_exploded_pubchem["CID"].astype("str")
         kegg_reaction_enzyme_df_exploded_pubchem["compound_reaction"] = kegg_reaction_enzyme_df_exploded_pubchem.apply(lambda x: [re.search(r"^(.+):", item)[1] for item in x.reaction_equation.split(",") if x.compound_id in item], axis = 1)
+        kegg_reaction_enzyme_df_exploded_pubchem["compound_reaction"] = kegg_reaction_enzyme_df_exploded_pubchem["compound_reaction"].str.join("|")
         kegg_reaction_enzyme_df_exploded_pubchem.to_pickle(f"{args.outdir}/kegg_reaction_enzyme_df_exploded_pubchem.pkl")
     else:
         print("PubChem records loaded from file")
@@ -629,6 +631,7 @@ def main():
         kegg_reaction_enzyme_df_exploded_gtc = kegg_reaction_enzyme_df_exploded_gtc.loc[kegg_reaction_enzyme_df_exploded_gtc.ROMol.isna() == False].reset_index(drop = True)
         kegg_reaction_enzyme_df_exploded_gtc["ligand_db"] = "GlyTouCan:" + kegg_reaction_enzyme_df_exploded_gtc["secondary_id"].astype("str")
         kegg_reaction_enzyme_df_exploded_gtc["compound_reaction"] = kegg_reaction_enzyme_df_exploded_gtc.apply(lambda x: [re.search(r"^(.+):", item)[1] for item in x.reaction_equation.split(",") if x.compound_id in item], axis = 1)
+        kegg_reaction_enzyme_df_exploded_gtc["compound_reaction"] = kegg_reaction_enzyme_df_exploded_gtc["compound_reaction"].str.join("|")
         kegg_reaction_enzyme_df_exploded_gtc.to_pickle(f"{args.outdir}/kegg_reaction_enzyme_df_exploded_gtc.pkl")
         print("GlyTouCan records saved")
     else:
@@ -680,25 +683,6 @@ def main():
         print("BRENDA cognate ligands loaded from file")
 
     if not os.path.exists(f"{args.outdir}/biological_ligands_df.pkl"):
-        descriptors = ['BalabanJ', 'BertzCT', 'Chi0', 'Chi0n', 'Chi0v', 'Chi1', 'Chi1n', 'Chi1v', 'Chi2n', 'Chi2v', 'Chi3n', 'Chi3v', 'Chi4n', 'Chi4v', 'EState_VSA1', 'EState_VSA10', 'EState_VSA11', 
-                'EState_VSA2', 'EState_VSA3', 'EState_VSA4', 'EState_VSA5', 'EState_VSA6', 'EState_VSA7', 'EState_VSA8', 'EState_VSA9', 'ExactMolWt', 'FpDensityMorgan1', 'FpDensityMorgan2', 
-                'FpDensityMorgan3', 'FractionCSP3', 'HallKierAlpha', 'HeavyAtomCount', 'HeavyAtomMolWt', 'Ipc', 'Kappa1', 'Kappa2', 'Kappa3', 'LabuteASA', 'MaxAbsEStateIndex', 'MaxAbsPartialCharge', 
-                'MaxEStateIndex', 'MaxPartialCharge', 'MinAbsEStateIndex', 'MinAbsPartialCharge', 'MinEStateIndex', 'MinPartialCharge', 'MolLogP', 'MolMR', 'MolWt', 'NHOHCount', 'NOCount', 
-                'NumAliphaticCarbocycles', 'NumAliphaticHeterocycles', 'NumAliphaticRings', 'NumAromaticCarbocycles', 'NumAromaticHeterocycles', 'NumAromaticRings', 'NumHAcceptors', 
-                'NumHDonors', 'NumHeteroatoms', 'NumRadicalElectrons', 'NumRotatableBonds', 'NumSaturatedCarbocycles', 'NumSaturatedHeterocycles', 'NumSaturatedRings', 'NumValenceElectrons', 
-                'PEOE_VSA1', 'PEOE_VSA10', 'PEOE_VSA11', 'PEOE_VSA12', 'PEOE_VSA13', 'PEOE_VSA14', 'PEOE_VSA2', 'PEOE_VSA3', 'PEOE_VSA4', 'PEOE_VSA5', 'PEOE_VSA6', 'PEOE_VSA7', 'PEOE_VSA8', 
-                'PEOE_VSA9', 'RingCount', 'SMR_VSA1', 'SMR_VSA10', 'SMR_VSA2', 'SMR_VSA3', 'SMR_VSA4', 'SMR_VSA5', 'SMR_VSA6', 'SMR_VSA7', 'SMR_VSA8', 'SMR_VSA9', 'SlogP_VSA1', 'SlogP_VSA10', 
-                'SlogP_VSA11', 'SlogP_VSA12', 'SlogP_VSA2', 'SlogP_VSA3', 'SlogP_VSA4', 'SlogP_VSA5', 'SlogP_VSA6', 'SlogP_VSA7', 'SlogP_VSA8', 'SlogP_VSA9', 'TPSA', 'VSA_EState1', 'VSA_EState10', 
-                'VSA_EState2', 'VSA_EState3', 'VSA_EState4', 'VSA_EState5', 'VSA_EState6', 'VSA_EState7', 'VSA_EState8', 'VSA_EState9', 'fr_Al_COO', 'fr_Al_OH', 'fr_Al_OH_noTert', 'fr_ArN', 
-                'fr_Ar_COO', 'fr_Ar_N', 'fr_Ar_NH', 'fr_Ar_OH', 'fr_COO', 'fr_COO2', 'fr_C_O', 'fr_C_O_noCOO', 'fr_C_S', 'fr_HOCCN', 'fr_Imine', 'fr_NH0', 'fr_NH1', 'fr_NH2', 'fr_N_O', 
-                'fr_Ndealkylation1', 'fr_Ndealkylation2', 'fr_Nhpyrrole', 'fr_SH', 'fr_aldehyde', 'fr_alkyl_carbamate', 'fr_alkyl_halide', 'fr_allylic_oxid', 'fr_amide', 'fr_amidine', 'fr_aniline', 
-                'fr_aryl_methyl', 'fr_azide', 'fr_azo', 'fr_barbitur', 'fr_benzene', 'fr_benzodiazepine', 'fr_bicyclic', 'fr_diazo', 'fr_dihydropyridine', 'fr_epoxide', 'fr_ester', 'fr_ether', 'fr_furan', 
-                'fr_guanido', 'fr_halogen', 'fr_hdrzine', 'fr_hdrzone', 'fr_imidazole', 'fr_imide', 'fr_isocyan', 'fr_isothiocyan', 'fr_ketone', 'fr_ketone_Topliss', 'fr_lactam', 'fr_lactone', 
-                'fr_methoxy', 'fr_morpholine', 'fr_nitrile', 'fr_nitro', 'fr_nitro_arom', 'fr_nitro_arom_nonortho', 'fr_nitroso', 'fr_oxazole', 'fr_oxime', 'fr_para_hydroxylation', 'fr_phenol', 
-                'fr_phenol_noOrthoHbond', 'fr_phos_acid', 'fr_phos_ester', 'fr_piperdine', 'fr_piperzine', 'fr_priamide', 'fr_prisulfonamd', 'fr_pyridine', 'fr_quatN', 'fr_sulfide', 'fr_sulfonamd', 
-                'fr_sulfone', 'fr_term_acetylene', 'fr_tetrazole', 'fr_thiazole', 'fr_thiocyan', 'fr_thiophene', 'fr_unbrch_alkane', 'fr_urea', 'qed']
-        # create molecular descriptor calculator
-        mol_descriptor_calculator = MolecularDescriptorCalculator(descriptors)
         biological_ligands_df = pd.concat([rhea_reactions[["entry", "compound_id", "compound_name", "ROMol", "ligand_db", "compound_reaction"]],
                                            kegg_reaction_enzyme_df_exploded_kegg[["entry", "compound_name", "compound_id", "ROMol", "ligand_db", "compound_reaction"]], 
                                            kegg_reaction_enzyme_df_exploded_chebi[["entry", "ChEBI_NAME", "KEGG COMPOUND ACCESSION", "ROMol", "ligand_db", "compound_reaction"]].rename(columns = {"KEGG COMPOUND ACCESSION" : "compound_id"}), 
@@ -738,9 +722,6 @@ def main():
         biological_ligands_df["isCofactor"] = biological_ligands_df["isCofactor"].fillna("N")
         biological_ligands_df.drop(columns = ["chebi_match", "FINAL_ID"], inplace = True)
 
-        #biological_ligands_mol_descriptors = biological_ligands_df['ROMol'].apply(calculate_descriptors)
-        #biological_ligands_mol_descriptors_df = pd.DataFrame(list(biological_ligands_mol_descriptors), columns=descriptors)
-        #biological_ligands_df = pd.concat([biological_ligands_df, biological_ligands_mol_descriptors_df], axis=1)
         biological_ligands_df.to_pickle(f"{args.outdir}/biological_ligands_df.pkl")
     else:
         biological_ligands_df = pd.read_pickle(f"{args.outdir}/biological_ligands_df.pkl")
