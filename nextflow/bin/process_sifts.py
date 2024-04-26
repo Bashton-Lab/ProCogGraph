79b8230c5fc98e0164feb808cddf3e9503cd08a6
m-crown <30960941+m-crown@users.noreply.github.com>
2024-04-26 20:52:20 +0100

    add assembly file for preferred struct

--- nextflow/bin/process_sifts.py
+++ nextflow/bin/process_sifts.py
@@ -1,16 +1,22 @@
+#/usr/bin/env python
 
+import pandas as pd
+import argparse
 
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--sifts_file', type=str, help='')
+    parser.add_argument('--assemblies_file', type=str, help='')
+    args = parser.parse_args()
 
-ec_records_df_grouped = process_ec_records(args.enzyme_dat_file , args.enzyme_class_file)
-ec_records_df = ec_records_df_grouped.explode("ID")
-sifts_chains_ec = sifts_chains.loc[sifts_chains.EC_NUMBER != "?"]
-sifts_chains_uniprot = sifts_chains.groupby(["PDB", "CHAIN"]).agg({"ACCESSION": set}).reset_index()
-sifts_chains_uniprot["ACCESSION"] = sifts_chains_uniprot["ACCESSION"].apply(lambda x: "|".join(x)) #join the list of uniprot accessions with a pipe for downstream neo4j integration
-sifts_chains_uniprot.rename(columns = {"ACCESSION" : "uniprot_accession"}, inplace = True)
+    sifts_file = pd.read_csv(args.sifts_file, sep = "\t", skiprows = 1)
+    assemblies_file = pd.read_csv(args.assemblies_file, usecols = ["ASSEMBLIES","PREFERED_ASSEMBLIES"]) #from https://ftp.ebi.ac.uk/pub/databases/pdbe-kb/complexes/, see https://www.biorxiv.org/content/10.1101/2023.05.15.540692v1.full
+    assemblies_file[["PDB", "ASSEMBLY_ID"]] = assemblies_file["ASSEMBLIES"].str.split("_", expand = True)
+    assemblies_file_prefered = assemblies_file.loc[assemblies_file.PREFERED_ASSEMBLIES == 1] #bool true
+    sifts_file_ec = sifts_file.loc[~sifts_file.EC_NUMBER.isin(["?"]), ["PDB"]].drop_duplicates()
+    sifts_assemblies = sifts_file_ec.merge(assemblies_file_prefered, how = "left", on = "PDB", indicator = True)
+    assert(len(sifts_assemblies.loc[sifts_assemblies._merge != "both"]) == 0)
+    sifts_assemblies.to_csv("sifts_assemblies.csv", index = False)
 
-sifts_chains_ec = sifts_chains_ec.groupby(["PDB"]).agg({"EC_NUMBER": set}).reset_index() #group these into a set of pdb associated ec's
-sifts_chains_ec["EC_NUMBER"] = sifts_chains_ec["EC_NUMBER"].apply(lambda x: ",".join(x)) #join the list of EC numbers into a single string for ec records function 
-sifts_chains_ec = get_updated_enzyme_records(sifts_chains_ec, ec_records_df, ec_col = "EC_NUMBER")
-sifts_chains_ec.rename(columns = {"EC_NUMBER": "protein_entity_ec", "ACCESSION" : "uniprot_accession"}, inplace = True)
-
-all_pdbs = sifts_chains_ec.PDB.unique()
+if __name__ == "__main__":
+    main()
